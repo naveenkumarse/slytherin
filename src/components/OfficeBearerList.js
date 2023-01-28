80c1e2adaac913f03b426859a7de9bfeb37faa73
Gokulkrishna Raju <gmkrishna11082003@gmail.com>
2023-01-28 23:44:52 +0530

    OB List

---
+++ src/components/OfficeBearerList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ProfileCard from './profileCard';
+
+const OfficeBearerList = () => {
+    return (
+
+    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300 '>
+
+        <ProfileCard/>
+        <ProfileCard/>
+        <ProfileCard/>
+        <ProfileCard/>
+        <ProfileCard/>
+        <ProfileCard/>
+        <ProfileCard/>
+
+
+    </div>
+        
+    );
+};
+
+export default OfficeBearerList;
