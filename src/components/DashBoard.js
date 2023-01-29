4bf98bc4a01defa2ba8e04ac47af05d4197d6a81
Naveenkumar S E <98598772+SENaveenkumar@users.noreply.github.com>
2023-01-29 23:03:33 +0530

    Corrections

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -19,7 +19,7 @@ const DashBoard = () => {
                         <div className="">
 
                             <Typed className="md:text-5xl font-mono sm:text-4xl text-xl font-bold" 
-                                strings={['Online registration open','Cash Prize upto ₹30K']}
+                                strings={['E-Registration open','Cash Prize upto ₹30K']}
                                 typeSpeed={90}
                                 backspeed={70}
                                 loop
